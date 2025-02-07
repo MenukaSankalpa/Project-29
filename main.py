8d0288b14ebe546433761379b3d842a3d36f80bd
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-07 07:51:28 +0530

    label created

--- main.py
+++ main.py
@@ -33,7 +33,20 @@ logo.place(x=150,y=100)
 #bottombox
 
 Frame_image=PhotoImage(file=r"Weatherapp\box.png")
-frame_myimage=Label(image=Frame_image)8
+frame_myimage=Label(image=Frame_image)
 frame_myimage.pack(padx=5,pady=5,side=BOTTOM)
+
+#label
+label1=Label(root,text="Wind",font=("Helvetica",15,"bold"),fg="white",bg="#1ab5ef")
+label1.place(x=120,y=400)
+
+label2=Label(root,text="Humidity",font=("Helvetica",15,"bold"),fg="white",bg="#1ab5ef")
+label2.place(x=250,y=400)
+
+label3=Label(root,text="Description",font=("Helvetica",15,"bold"),fg="white",bg="#1ab5ef")
+label3.place(x=430,y=400)
+
+label4=Label(root,text="Pressure",font=("Helvetica",15,"bold"),fg="white",bg="#1ab5ef")
+label4.place(x=650,y=400)
 
 root.mainloop()
